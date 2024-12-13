624ceb4c0c399377e686938790c6f73a823f2c59
Ubuntu <Boris Malgin>
2024-12-13 04:39:09 +0000

    Матричный Профиль-ОТЛ

--- practice/04 Matrix profile/modules/mp.py
+++ practice/04 Matrix profile/modules/mp.py
@@ -1,12 +1,14 @@
-import numpy as np
-import pandas as pd
 import math
 
+import numpy as np
+import pandas as pd
 import stumpy
 from stumpy import config
 
 
-def compute_mp(ts1: np.ndarray, m: int, exclusion_zone: int = None, ts2: np.ndarray = None):
+def compute_mp(
+    ts1: np.ndarray, m: int, exclusion_zone: int = None, ts2: np.ndarray = None
+):
     """
     Compute the matrix profile
 
@@ -20,18 +22,20 @@ def compute_mp(ts1: np.ndarray, m: int, exclusion_zone: int = None, ts2: np.ndar
     Returns
     -------
     output: the matrix profile structure
-            (matrix profile, matrix profile index, subsequence length, exclusion zone, the first and second time series)
+                                    (matrix profile, matrix profile index, subsequence length, exclusion zone, the first and second time series)
     """
-    
-    #mp = stumpy.stump(ts1, m, exclusion_zone=exclusion_zone)
-    
-    ts1 = ts1[1]  # Выбор первого столбца
-    ts2 = ts2[1]
-    mp = stumpy.stump(T_A=ts1, T_B=ts2, m=m, ignore_trivial=False)
 
-    return {'mp': mp[:, 0],
-            'mpi': mp[:, 1],
-            'm' : m,
-            'excl_zone': exclusion_zone,
-            'data': {'ts1' : ts1, 'ts2' : ts2}
-            }
+    data = {"ts1": ts1}
+    if ts2 is None:
+        mp = stumpy.stump(ts1, m)
+    else:
+        data |= {"ts2": ts2}
+        mp = stumpy.stump(T_A=ts1[1], m=m, T_B=ts2[1], ignore_trivial=False)
+
+    return {
+        "mp": mp[:, 0],
+        "mpi": mp[:, 1],
+        "m": m,
+        "excl_zone": exclusion_zone,
+        "data": {"ts1": ts1},
+    }
